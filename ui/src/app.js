d4ea1a0d44babf4757b8cdfaa2a1a36f477abf32
touchskyer <touchskyer@touchskyerMacbookPro.local>
2018-02-07 23:27:00 +0800

    Introduced echarts and react-for-echarts
    Quickly added one stand up demo

--- ui/src/app.js
+++ ui/src/app.js
@@ -1,28 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom';
+import echarts from 'echarts';
+import ReactEcharts from 'echarts-for-react';
+import daoTheme1 from './assets/DaoTheme1.json';
 import Hello from './component/hello';
 
 
-export default function App() {
-  return (
-    <HashRouter>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          position: 'fixed',
-          height: '100%',
-          width: '100%',
-          marginTop: '20px',
-          overflow: 'auto',
-        }}
-      >
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    echarts.registerTheme('theme1', daoTheme1);
+  }
+
+  render() {
+    const option = {
+      title: {
+        text: 'ECharts 入门示例',
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量', '预测'],
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20],
+      }, {
+        name: '预测',
+        type: 'line',
+        data: [5, 20, 36, 10, 10, 20],
+      }],
+    };
+
+    return (
+      <HashRouter>
         <div
-          style={{ flexBasis: 0, flexGrow: 1, width: '100%' }}
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            position: 'fixed',
+            height: '100%',
+            width: '100%',
+            marginTop: '20px',
+            overflow: 'auto',
+          }}
         >
-          <Route exact path="/" component={Hello} />
+          <div
+            style={{ flexBasis: 0, flexGrow: 1, width: '100%' }}
+          >
+            <Route exact path="/" component={Hello} />
+          </div>
+
+          <div
+            style={{ flexBasis: 0, flexGrow: 1, width: '100%' }}
+          >
+            <ReactEcharts
+              option={option}
+              theme="theme1"
+              style={{ height: '400px', width: '100%' }}
+            />
+          </div>
+          <div
+            style={{ flexBasis: 0, flexGrow: 1, width: '100%' }}
+          >
+            <ReactEcharts
+              option={option}
+              theme="theme1"
+              style={{ height: '400px', width: '100%' }}
+            />
+          </div>
         </div>
-      </div>
-    </HashRouter>
-  );
+      </HashRouter>
+    );
+  }
 }
