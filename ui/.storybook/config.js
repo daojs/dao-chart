2b0c615ed25825acae87a834ed135d7a426b5b9e
suro1995 <suro1995@gmail.com>
2018-02-23 00:44:31 +0800

    add word cloud chart

--- ui/.storybook/config.js
+++ ui/.storybook/config.js
@@ -2,6 +2,7 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../stories/index.js');
+  require('../stories/wordcloud.js');
   // You can require as many stories as you need.
 }
 
