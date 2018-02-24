4c6635779b669573cc71dc67bd019aa9f1e1e751
Yucong Jin <yucongj@microsoft.com>
2018-02-23 22:55:46 -0800

    refactor structure

--- mt/schema.js
+++ mt/schema.js
@@ -1,43 +1,17 @@
 const _ = require('lodash');
-
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString,
-  GraphQLList
+  GraphQLString
 } = require('graphql');
+
+const eunmTypeType = require('./types/enum-type');
 
 const mockTypes = [
   { id: '1', name: 'Beijing' },
   { id: '2', name: 'wewei@microsoft.com', parent: '1' },
   { id: '3', name: 'beef', relation: ['1', '2'] }
 ];
-
-const connectionType = new GraphQLObjectType({
-  name: 'Connection',
-  fields: {
-    id: { type: GraphQLString },
-    name: { type: GraphQLString }
-  },
-  resolve(parent, { id }) {
-    return {
-      id: id,
-      name: JSON.stringify(parent)
-    };
-  }
-});
-
-const eunmTypeType = new GraphQLObjectType({
-  name: 'enumType',
-  fields: {
-    id: { type: GraphQLString },
-    name: { type: GraphQLString },
-    relation: {
-      type: new GraphQLList(connectionType)
-    },
-    parent: { type: connectionType }
-  }
-});
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
