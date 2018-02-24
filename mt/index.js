185145b89e27948d99ac6fdd344a5d69f54b2a06
Yucong Jin <yucongj@microsoft.com>
2018-02-23 21:21:29 -0800

    eslint and graphql

--- mt/index.js
+++ mt/index.js
@@ -1,18 +1,21 @@
+
 const Koa = require('koa');
 const serve = require('koa-static');
 const Router = require('koa-router');
+const graphqlHTTP = require('koa-graphql');
+
+const schema = require('./schema');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/line/mock', (ctx, next) => {
-  const mockLineData = require('../mocks/line');
+router.all('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
 
-  ctx.body = mockLineData;
-  return next();
-});
+app.use(router.routes()).use(router.allowedMethods());
 
-app.use(router.prefix('/api').routes());
 app.use(serve('../target'));
 
 app.listen(9001);
