3ab94ba785f6ab3bf1a6d692c8b924f2cd1b7e51
Ling Zhou <lizho@microsoft.com>
2018-02-24 02:25:09 -0800

    Fix comments

--- ui/stories/bubble.stories.js
+++ ui/stories/bubble.stories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { storiesOf } from '../.storybook/facade';
 import Bubble from '../src/component/bubble';
 
 // Income mapx to x, Age maps to y, GDP maps to size, Country maps to label, Year maps to legend
