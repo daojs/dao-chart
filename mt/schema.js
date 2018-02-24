b76fe16a6813be80a80cf14f2bc5a92f3ea02653
Yucong Jin <yucongj@microsoft.com>
2018-02-23 22:18:30 -0800

    enumTypes

--- mt/schema.js
+++ mt/schema.js
@@ -1,17 +1,41 @@
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLList
 } = require('graphql');
+
+const mockTypes = [
+  { id: '1', name: 'Beijing' },
+  { id: '2', name: 'wewei@microsoft.com', parent: [ '1' ] },
+  { id: '3', name: 'beef', relation: ['1', '2'] }
+];
+
+const eunmTypeType = new GraphQLObjectType({
+  name: 'enumType',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    parent: {
+      type: new GraphQLList(GraphQLString)
+    },
+    relation: {
+      type: new GraphQLList(GraphQLString)
+    }
+  }
+});
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-      hello: {
-        type: GraphQLString,
-        resolve() {
-          return 'world';
+      enumType: {
+        type: eunmTypeType,
+        args: {
+          id: { type: GraphQLString }
+        },
+        resolve(parent, { id }) {
+          return mockTypes.find(t => t.id === id);
         }
       }
     }
