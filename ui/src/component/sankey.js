c0db2e411a27f6f8065305a54595ce961dba8542
suro1995 <yuzhon@microsoft.com>
2018-02-27 00:32:50 +0800

    Add sankey chart

---
+++ ui/src/component/sankey.js
@@ -0,0 +1,52 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import ReactEcharts from 'echarts-for-react';
+import _ from 'lodash';
+import { validate, getDataOption } from '../utils';
+
+export default class Sankey extends PureComponent {
+  static propTypes = {
+    source: PropTypes.arrayOf(PropTypes.array).isRequired,
+  }
+
+  render() {
+    const { source } = this.props;
+    validate(source);
+
+    const option = {
+      tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove',
+      },
+      series: {
+        type: 'sankey',
+        layout: 'none',
+        data: _.chain(source)
+          .first()
+          .slice(1)
+          .map(name => ({ name }))
+          .value(),
+        links: (() => {
+          const links = [];
+
+          for (let i = 1; i < source.length; i += 1) {
+            for (let j = 1; j < source.length; j += 1) {
+              if (source[i][j] > 0) {
+                links.push({
+                  source: source[0][i],
+                  target: source[0][j],
+                  value: source[i][j],
+                });
+              }
+            }
+          }
+          return links;
+        })(),
+      },
+    };
+
+    return (
+      <ReactEcharts option={option} {...this.props} />
+    );
+  }
+}
