b063ae72342948467a01edaecbb7efcabe553025
Yucong Jin <yucongj@microsoft.com>
2018-03-06 05:33:38 -0800

    TEST

--- mt/test/index.test.js
+++ mt/test/index.test.js
@@ -2,7 +2,62 @@
 const request = require('supertest');
 const app = require('../index');
 
-describe('App', () => {
+describe('Data Query', () => {
+  afterEach(done => {
+    app.close();
+    done();
+  });
+
+  it('should work for single query', done => {
+    const requestBody = `
+    {
+      "name": "query",
+      "parameters": {
+        "metrics": "169feb7a-c332-4979-9c6a-377bf2d75152",
+        "dimensions": [["between", "time", "2017-12-01", "2017-12-02"], ["in", "BranchName", ["AllUp"]], ["in", "CardType", ["访客卡"]], ["eq", "MealName", "晚餐"]]}
+    }
+    `;
+
+    request(app)
+      .post('/graphql')
+      .send(requestBody)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(() => {
+        done();
+      });
+  });
+
+  it('should work for batch query', done => {
+    const requestBody = `
+    [
+      {
+        "name": "query",
+        "parameters": {
+          "metrics": "169feb7a-c332-4979-9c6a-377bf2d75152",
+          "dimensions": [["between", "time", "2017-12-01", "2017-12-02"], ["in", "BranchName", ["AllUp"]], ["in", "CardType", ["访客卡"]], ["eq", "MealName", "晚餐"]]}
+      },
+      {
+        "name": "query",
+        "parameters": {
+          "metrics": "169feb7a-c332-4979-9c6a-377bf2d75152",
+          "dimensions": [["between", "time", "2017-12-01", "2017-12-02"], ["in", "BranchName", ["AllUp"]], ["in", "CardType", ["访客卡"]], ["eq", "MealName", "晚餐"]]}
+      }
+    ]
+    `;
+
+    request(app)
+      .post('/graphql')
+      .send(requestBody)
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(() => {
+        done();
+      });
+  });
+});
+
+describe('GraphQL', () => {
   afterEach(done => {
     app.close();
     done();
