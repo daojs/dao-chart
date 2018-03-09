cf05ee7915b2e43dac5a95aa10251ae91290c6c0
Yucong Jin <yucongj@microsoft.com>
2018-03-08 22:10:00 -0800

    refactor logger

--- mt/index.js
+++ mt/index.js
@@ -5,27 +5,18 @@ const graphqlHTTP = require('koa-graphql');
 const compress = require('koa-compress');
 const Boom = require('boom');
 const koaBody = require('koa-body');
-const log4js = require('log4js');
 
 const schema = require('./schema');
+
+// middlewares
 const errorHandler = require('./middlewares/error-handler');
 const data = require('./middlewares/data');
-
-log4js.configure({
-  appenders: {
-    query: { type: 'file', filename: `./logs/query-${new Date().toLocaleDateString()}.log` },
-    common: { type: 'file', filename: `./logs/common-${new Date().toLocaleDateString()}.log` }
-  },
-  categories: {
-    default: { appenders: ['common'], level: 'info' },
-    common: { appenders: ['common'], level: 'info' },
-    query: { appenders: ['query'], level: 'info' }
-  }
-});
+const logger = require('./middlewares/logger');
 
 const app = new Koa();
 const router = new Router();
 
+app.use(logger());
 app.use(errorHandler());
 
 router.all('/graphql', graphqlHTTP({
