a9cbeb3c63f4c6d6cee341e018b3374dc1e84938
Zhibin Liu <ljqx@live.cn>
2018-03-23 14:13:36 +0800

    [Insight Chart] partition chart

--- ui/stories/pie.stories.js
+++ ui/stories/pie.stories.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { storiesOf } from '../.storybook/facade';
 import Pie from '../src/component/pie';
+import Partition from '../src/component/partition';
 import { pieSource } from './data';
 
+const partitionSource = [
+  ['Branch', 'Revenue'],
+  ['员工餐厅', 9711],
+  ['自助餐', 971],
+  ['咖喱屋', 97],
+];
 
 storiesOf('Pie Chart', module)
   .add('pie chart', () => (
     <Pie source={pieSource} />
+  ))
+  .add('Partition', () => (
+    <Partition source={partitionSource} />
   ));
